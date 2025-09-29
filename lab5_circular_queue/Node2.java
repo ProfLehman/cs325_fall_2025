1eeb03be3b29b0f11662823a2455825dbfbd6788
Jeff Lehman <jlehman@huntington.edu>
2025-09-29 12:21:26 -0400

    updating limit example for dynamic list

--- lab5_circular_queue/Node2.java
+++ lab5_circular_queue/Node2.java
@@ -8,20 +8,23 @@
     Stores int data
 */
 public class Node2 {
-    ;
+
     public int data;
     public Node2 next;
+    public int[] arr;
 
     // default constructor
     public Node2() {
-        data = -1;
-        next = null;
+        this.data = -1;
+        this.next = null;
+        this.arr = new int[100000000]; // array of ints
     }
 
     // alternate constructor
     public Node2(int value) {
         data = value;
         this.next = null;
+        this.arr = new int[100000000]; // array of ints
     }
 
 }
---
+++ lab5_circular_queue/TestLimitLinkedList.java
@@ -0,0 +1,57 @@
+// TestLimitLinkedList.java
+// fall 2025
+// prof. lehman
+//
+// demonstrates running out of heap memory
+// note: modify Node.java to include an array of ints
+// note: show memory performance for OS to watch memory allocation
+//       in Windows, task manager, performance, memory
+
+// -Xms is starting heap size
+// -Xmx is maximum heap size
+
+// java -Xms2g -Xmx4g TestLimitLinkedList
+// java -Xms4g -Xmx6g TestLimitLinkedList
+
+import java.util.Random;
+
+public class TestLimitLinkedList {
+
+    public static void main(String[] args) {
+
+        int maxValue = 1000000000;
+
+        Random rand = new Random();
+
+        SortedLinkedList linkedList = new SortedLinkedList();
+
+        int count = 0;
+        while (true) {
+
+            linkedList.insert(rand.nextInt(maxValue)); // insert random number
+
+            count++;
+            System.out.println(count);
+
+            printHeapUsage();
+        }
+
+    }// main
+
+    private static void printHeapUsage() {
+        Runtime rt = Runtime.getRuntime();
+        long total = rt.totalMemory();
+        long free = rt.freeMemory();
+        long used = total - free;
+        long max = rt.maxMemory();
+
+        System.err.printf("Heap used: %,d bytes (%.2f MB)%n", used, bytesToMB(used));
+        System.err.printf("Heap total (committed): %,d bytes (%.2f MB)%n", total, bytesToMB(total));
+        System.err.printf("Heap max (-Xmx): %,d bytes (%.2f MB)%n", max, bytesToMB(max));
+    }
+
+    private static double bytesToMB(long b) {
+        return b / (1024.0 * 1024.0);
+    }
+
+} // class
